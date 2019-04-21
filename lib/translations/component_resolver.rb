4a0e91edaf722d6660522b0e11d06cf93908c793
josediaz16 <jldiaz1612@gmail.com>
2019-04-20 20:30:26 -0500

    adds translations resolver

---
+++ lib/translations/component_resolver.rb
@@ -0,0 +1,18 @@
+module Translations
+  ComponentResolver = -> root, translations = nil do
+    translations ||= I18n.t(root)
+
+    translations.each_with_object({}) do |(key, value), hash|
+      case value
+      when String, Symbol
+        if value.to_s.match(/components\.\w+/).present?
+          hash[key] = I18n.t("#{root}.#{key}")
+        else
+          hash[key] = value
+        end
+      when Hash
+        hash[key] = ComponentResolver.(root, value)
+      end
+    end
+  end
+end
