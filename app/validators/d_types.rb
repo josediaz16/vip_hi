6d8d09a8baf5c8229f7b255e8881a770dcc0e5a3
josediaz16 <jldiaz1612@gmail.com>
2019-05-01 13:51:41 -0500

    refactor validator

--- app/validators/d_types.rb
+++ app/validators/d_types.rb
@@ -1,5 +1,10 @@
 module Validators
   module DTypes
     include Dry::Types.module
+    DB_PHONE_FORMAT = /(?:\+\d{2}(?: |)|[\D])/
+
+    Phone = DTypes::String.constructor do |value|
+      value.gsub(DB_PHONE_FORMAT, "")
+    end
   end
 end
--- app/validators/users.rb
+++ app/validators/users.rb
@@ -6,21 +6,26 @@ module Validators
         config.namespace = :user
       end
 
-      required(:email).filled(:str?, :email?)
-      required(:password).filled(:str?, min_size?: 8).confirmation
-      required(:country_id).filled(:int?)
-      optional(:phone).filled(:phone?)
-      optional(:id).filled
+      required(:email,      DTypes::String).filled(:email?)
+      required(:password,   DTypes::String).filled(min_size?: 8)
+      required(:password_confirmation, DTypes::String).filled
+      required(:country_id, DTypes::Integer).filled
+
+      optional(:phone, DTypes::Phone).filled(:phone?)
+      optional(:id,    DTypes::Integer).filled
+      rule(password_confirmation: [:password, :password_confirmation]) do |pass, confirmation|
+        pass.eql?(confirmation)
+      end
     end
 
     CreateCelebrity = Builder.(Base) do
-      optional(:name).filled(:str?)
-      required(:known_as).filled(:str?)
-    end
+      required(:known_as, DTypes::String).filled
+      optional(:name,     DTypes::String).filled
+    end.with(object_class: :user)
 
     CreateFan = Builder.(Base) do
-      required(:name).filled(:str?)
-      optional(:known_as).filled(:str?)
-    end
+      required(:name,       DTypes::String).filled
+      optional(:known_as, DTypes::String).filled
+    end.with(object_class: :user)
   end
 end
