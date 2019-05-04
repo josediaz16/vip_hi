b9352aaa1b2f87a903445e0cdc45f3d7d6a9e6ee
josediaz16 <jldiaz1612@gmail.com>
2019-05-04 12:49:27 -0500

    adds PhoneNumberInput component

---
+++ app/javascript/components/inputs/PhoneNumberInput.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PhoneInput from 'react-phone-number-input'
+
+import 'react-phone-number-input/style.css'
+
+class PhoneNumberInput extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      phone: "",
+      country: ""
+    }
+
+    this.onChange = this.onChange.bind(this)
+  }
+
+  findCountry() {
+    return document.querySelector(`select[name='${this.props.name}__country']`).value
+  }
+
+  onChange(phone) {
+    const country = this.findCountry()
+    this.setState({phone, country})
+  }
+
+  render() {
+    const {
+      countryInputName,
+      ...otherProps
+    } = this.props
+
+    return (
+      <React.Fragment>
+        <PhoneInput
+          country="CO"
+          value={this.state.phone}
+          onChange={this.onChange}
+          {...otherProps}
+        />
+        <input type="hidden" value={this.state.country} name={countryInputName}/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default PhoneNumberInput
--- app/javascript/components/inputs/index.js
+++ app/javascript/components/inputs/index.js
@@ -1,10 +1,12 @@
 import NativeInput            from 'components/inputs/NativeInput'
+import PhoneNumberInput       from 'components/inputs/PhoneNumberInput'
 import Label                  from 'components/inputs/Label'
 import Icon                   from 'components/inputs/Icon'
 import Hint                   from 'components/inputs/Hint'
 
 export {
   NativeInput,
+  PhoneNumberInput,
   Label,
   Icon,
   Hint
