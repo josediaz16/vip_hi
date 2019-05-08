cef37f2efc4ebe4ee1a3ef8be589419650641cee
josediaz16 <jldiaz1612@gmail.com>
2019-05-08 10:22:34 -0500

    config letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   devise_for :users, controllers: {registrations: "users/registrations", confirmations: "users/confirmations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
