38b00149b7463dc248f6bb5f99d141c1e1aaba70
josediaz16 <jldiaz1612@gmail.com>
2019-06-18 18:07:40 -0500

    adds missing translations for flashes

--- app/javascript/components/Alert.js
+++ app/javascript/components/Alert.js
@@ -4,19 +4,36 @@ import Icon from 'components/inputs/Icon'
 import classNames from 'classnames'
 
 class Alert extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      active: true
+    }
+  }
+
+  onClose() {
+    if (this.props.onClose) {
+      this.props.onClose()
+    }
+    else {
+      this.setState({active: false})
+    }
+  }
+
   render() {
     const { alertClass, message, onClose } = this.props
     const className = classNames("alert", alertClass, "fade in")
 
     return (
-      <div className="alert-wrapper">
-        <div className={className} role="alert">
-          <p>{message}</p>
-          <button className="close" type="button" onClick={onClose}>
-            <Icon icon="cancel"/>
-          </button>
-        </div>
-      </div>
+      this.state.active ?
+        <div className="alert-wrapper">
+          <div className={className} role="alert">
+            <p>{message}</p>
+            <button className="close" type="button" onClick={this.onClose.bind(this)}>
+              <Icon icon="cancel"/>
+            </button>
+          </div>
+        </div> : null
     )
   }
 }
