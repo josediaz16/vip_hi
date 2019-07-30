c27f34a222b97c4dfa9f022e090e250a45bd6a86
josediaz16 <jldiaz1612@gmail.com>
2019-07-30 11:42:48 -0500

    update mr state machine

--- app/state_machines/mr_state_machine.rb
+++ app/state_machines/mr_state_machine.rb
@@ -3,12 +3,14 @@ class MRStateMachine
   include Statesman::Machine
 
   state :pending, initial: true
+  state :purchased
   state :accepted
   state :rejected
   state :cancelled
   state :delivered
 
-  transition from: :pending,   to: [:accepted, :rejected, :cancelled]
+  transition from: :pending,   to: [:purchased]
+  transition from: :purchased, to: [:accepted, :rejected, :cancelled, :delivered]
   transition from: :accepted,  to: [:cancelled, :delivered]
   transition from: :cancelled, to: [:pending]
 end
