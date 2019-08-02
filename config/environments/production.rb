e2b8586c3f0de9fae2c516217a2fcca5889976ac
josediaz16 <jldiaz1612@gmail.com>
2019-08-01 20:17:56 -0500

    configure smtp prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => "testingsf.tk"}
+  config.action_mailer.asset_host = "https://testingsf.tk"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    authentication:       :plain,
+    domain:               'testingsf.tk',
+    port:                 '587',
+    address:              'smtp.sendgrid.net',
+    user_name:            ENV['MAILER_USER'],
+    password:             ENV['MAILER_PASSWORD']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
