e5d558a375869a5510d459dd3bff3123608a2777
josediaz16 <jldiaz1612@gmail.com>
2019-08-08 14:43:03 -0500

    update domain config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => "testingsf.tk"}
-  config.action_mailer.asset_host = "https://testingsf.tk"
+  config.action_mailer.default_url_options = { :host => "saludofamosos.com"}
+  config.action_mailer.asset_host = "https://saludofamosos.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
     authentication:       :plain,
-    domain:               'testingsf.tk',
+    domain:               'saludofamosos.com',
     port:                 '587',
     address:              'smtp.sendgrid.net',
     user_name:            ENV['MAILER_USER'],
